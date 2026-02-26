23686afee5c5c5e21a8f1a0c8dc606c91d3a5582
ZikannSenpai <zidanriski778@gmail.com>
2026-02-27 04:53:08 +0700

    add route

--- pages/api/lastup.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({
-        commit: process.env.VERCEL_GIT_COMMIT_SHA || "dev",
-        message: process.env.VERCEL_GIT_COMMIT_MESSAGE || "local",
-        date: new Date().toISOString()
-    });
-}
