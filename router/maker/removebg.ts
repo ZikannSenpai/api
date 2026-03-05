19b65b8ceba9a0a5f6307be65bf57d14c904073e
ZikannSenpai <zidanriski778@gmail.com>
2026-03-05 17:37:14 +0700

    add route

--- router/maker/removebg.ts
+++ router/maker/removebg.ts
@@ -12,20 +12,20 @@ export default async function removeBG(req: Request, res: Response) {
     }
 
     try {
-        const link = `https://api.danzy.web.id/api/maker/removebg?url=${encodeURIComponent(url)}`;
+        const api = `https://api.danzy.web.id/api/maker/removebg?url=${encodeURIComponent(url)}`;
 
-        const data = await axios.get(link, {
+        const response = await axios.get(api, {
             responseType: "arraybuffer",
             headers: {
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                Referer: "https://api.danzy.web.id/",
-                Connection: "keep-alive"
+                "User-Agent": "Mozilla/5.0"
             }
         });
-        res.set("Content-Type", "image/png");
-        res.send(data.data);
+
+        res.set(
+            "Content-Type",
+            response.headers["content-type"] || "image/png"
+        );
+        res.send(response.data);
     } catch (err: any) {
         res.status(500).json({
             status: false,
