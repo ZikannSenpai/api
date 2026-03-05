b7b1ea742c2b8722b83a669a7fc575d67d59efe4
ZikannSenpai <zidanriski778@gmail.com>
2026-03-05 16:40:20 +0700

    add route

--- router/maker/removebg.ts
+++ router/maker/removebg.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import axios from "axios";
 
-export default async function detailAnime(req: Request, res: Response) {
+export default async function removeBG(req: Request, res: Response) {
     const { url } = req.url;
 
     if (!url) {
         return res.status(400).json({
             status: false,
-            message: "Parameter 'slug' diperlukan."
+            message: "Parameter 'url' diperlukan."
         });
     }
 
@@ -16,8 +16,7 @@ export default async function detailAnime(req: Request, res: Response) {
 
         const { data } = await axios.get(url, {
             headers: {
-                "User-Agent": "Mozilla/5.0",
-                Referer: "https://sankavollerei.com/"
+                "User-Agent": "Mozilla/5.0"
             }
         });
         res.json({
