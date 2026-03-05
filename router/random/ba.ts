0e169fdd874439f2f33d600bb411b6605851227b
ZikannSenpai <zidanriski778@gmail.com>
2026-03-05 18:04:02 +0700

    add route

--- router/random/ba.ts
+++ router/random/ba.ts
@@ -6,16 +6,10 @@ export default async function removeBG(req: Request, res: Response) {
         const api = `https://api.danzy.web.id/api/random/blue-archive`;
 
         const response = await axios.get(api, {
-            responseType: "arraybuffer",
             headers: {
                 "User-Agent": "Mozilla/5.0"
             }
         });
-
-        res.set(
-            "Content-Type",
-            response.headers["content-type"] || "image/png"
-        );
         res.send(response);
     } catch (err: any) {
         res.status(500).json({
