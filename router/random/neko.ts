ab1be1d6cb089041dca3022f4aad9fcfe612fe1c
ZikannSenpai <zidanriski778@gmail.com>
2026-03-05 17:56:40 +0700

    add route

---
+++ router/random/neko.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import axios from "axios";
+
+export default async function removeBG(req: Request, res: Response) {
+    try {
+        const api = `https://api.danzy.web.id/api/random/neko`;
+
+        const response = await axios.get(api, {
+            responseType: "arraybuffer",
+            headers: {
+                "User-Agent": "Mozilla/5.0"
+            }
+        });
+
+        res.set(
+            "Content-Type",
+            response.headers["content-type"] || "image/png"
+        );
+        res.send(response.data);
+    } catch (err: any) {
+        res.status(500).json({
+            status: false,
+            message: err.message
+        });
+    }
+}
