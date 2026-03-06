6a7fab3d40b52b3454ae112aaae838ec657a4870
ZikannSenpai <zidanriski778@gmail.com>
2026-03-06 23:35:38 +0700

    add route

--- router/search/spotify.ts
+++ router/search/spotify.ts
@@ -14,7 +14,7 @@ export default async function spotifys(req: Request, res: Response) {
     try {
         const url = `https://api.danzy.web.id/api/search/spotify?q=${slug}`;
 
-        const data = await axios.get(url, {
+        const { data } = await axios.get(url, {
             headers: {
                 "User-Agent":
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36",
